9b53364098838f479a05d3cf078de5b31b0a2e76
Maxime De Greve <maximedegreve@me.com>
2023-11-01 18:36:40 +0000

    Fix

--- src/TableV1.js
+++ src/TableV1.js
@@ -14,124 +14,125 @@ import {
 
 function TableV1() {
     return (
-            <Box
-                sx={{
-                    display: 'grid',
-                    gridTemplateColumns: [
-                        'auto auto 1fr 0px auto',
-                        'auto auto 1fr 0px auto',
-                        'auto auto 1fr max(30%) auto auto auto auto',
-                    ],
-                    bg: 'red',
-                    width: '100%',
-                    maxWidth: 1400,
-                    mx: 'auto',
-                    borderColor: 'border.default',
-                    borderRadius: 2,
-                    borderWidth: 1,
-                    overflow: 'hidden',
-                    borderStyle: 'solid',
-                    boxShadow: 'shadow.small',
-                }}
-            >
-                <Row
-                    title="AvatarStack disableExpand and rightAlign don't work together"
-                    hash={3884}
-                    totalComments={0}
-                    totalPullRequests={0}
-                    labels={[{ name: 'bug' }, { name: 'react' }]}
-                    avatars={[
-                        {
-                            alt: 'GitHub logo',
-                            src: 'https://avatars.githubusercontent.com/github',
-                        },
-                    ]}
-                />
-                <Row
-                    title="ActionMenu: Selected items use checkboxes instead of checkmarks"
-                    hash={3878}
-                    totalComments={10}
-                    totalPullRequests={0}
-                    labels={[
-                        { name: 'bug' },
-                        { name: 'component: ActionMenu' },
-                        { name: 'react' },
-                    ]}
-                    avatars={[
-                        {
-                            alt: 'GitHub logo',
-                            src: 'https://avatars.githubusercontent.com/github',
-                        },
-                    ]}
-                />
+        <Box
+            as="ul"
+            sx={{
+                display: 'grid',
+                gridTemplateColumns: [
+                    'auto auto 1fr 0px auto',
+                    'auto auto 1fr 0px auto',
+                    'auto auto 1fr max(30%) auto auto auto auto',
+                ],
+                bg: 'red',
+                width: '100%',
+                maxWidth: 1400,
+                mx: 'auto',
+                borderColor: 'border.default',
+                borderRadius: 2,
+                borderWidth: 1,
+                overflow: 'hidden',
+                borderStyle: 'solid',
+                boxShadow: 'shadow.small',
+            }}
+        >
+            <Row
+                title="AvatarStack disableExpand and rightAlign don't work together"
+                hash={3884}
+                totalComments={0}
+                totalPullRequests={0}
+                labels={[{ name: 'bug' }, { name: 'react' }]}
+                avatars={[
+                    {
+                        alt: 'GitHub logo',
+                        src: 'https://avatars.githubusercontent.com/github',
+                    },
+                ]}
+            />
+            <Row
+                title="ActionMenu: Selected items use checkboxes instead of checkmarks"
+                hash={3878}
+                totalComments={10}
+                totalPullRequests={0}
+                labels={[
+                    { name: 'bug' },
+                    { name: 'component: ActionMenu' },
+                    { name: 'react' },
+                ]}
+                avatars={[
+                    {
+                        alt: 'GitHub logo',
+                        src: 'https://avatars.githubusercontent.com/github',
+                    },
+                ]}
+            />
 
-                <Row
-                    title="TextInput action hover state is incorrect"
-                    hash={3867}
-                    totalComments={10}
-                    totalPullRequests={0}
-                    labels={[
-                        { name: 'bug' },
-                        { name: 'component: TextInput' },
-                        { name: 'react' },
-                    ]}
-                    avatars={[
-                        {
-                            alt: 'GitHub logo',
-                            src: 'https://avatars.githubusercontent.com/u/68850280?s=64&v=4',
-                        },
-                    ]}
-                />
+            <Row
+                title="TextInput action hover state is incorrect"
+                hash={3867}
+                totalComments={10}
+                totalPullRequests={0}
+                labels={[
+                    { name: 'bug' },
+                    { name: 'component: TextInput' },
+                    { name: 'react' },
+                ]}
+                avatars={[
+                    {
+                        alt: 'GitHub logo',
+                        src: 'https://avatars.githubusercontent.com/u/68850280?s=64&v=4',
+                    },
+                ]}
+            />
 
-                <Row
-                    title="Data table pagination breaks when setting defaultPageIndex"
-                    hash={3856}
-                    totalComments={5}
-                    totalPullRequests={1}
-                    labels={[
-                        { name: 'bug' },
-                        { name: 'component: DataTable' },
-                        { name: 'good first issue' },
-                        { name: 'react' },
-                        { name: 'size: sand' },
-                    ]}
-                    avatars={[
-                        {
-                            alt: 'GitHub logo',
-                            src: 'https://avatars.githubusercontent.com/u/431533?s=64&u=9e71f9aaba1bd41361ee043ef018165b723ffc3e&v=4',
-                        },
-                    ]}
-                />
-                <Row
-                    title="Release Tracking"
-                    hash={3856}
-                    totalComments={5}
-                    totalPullRequests={1}
-                    labels={[
-                        { name: 'bug' },
-                        { name: 'release' },
-                        { name: 'update' },
-                        { name: 'react' },
-                        { name: 'size: boulder' },
-                        { name: 'warning' },
-                        { name: 'deprecation' },
-                    ]}
-                    avatars={[
-                        {
-                            alt: 'GitHub logo',
-                            src: 'https://avatars.githubusercontent.com/u/1863771?s=64&u=36c77cc9be0a64a196b503ce5e7fc335912d2bfa&v=4',
-                        },
-                        {
-                            alt: 'GitHub logo',
-                            src: 'https://avatars.githubusercontent.com/u/980622?s=64&u=1c3bf339aa927382a4f123b7b3669f847b1535e4&v=4',
-                        },
-                        {
-                            alt: 'GitHub logo',
-                            src: 'https://avatars.githubusercontent.com/u/40274682?s=64&u=e86e9e13f63066ced355e03ed21e05f91915567a&v=4',
-                        },
-                    ]}
-                />
-            </Box>
+            <Row
+                title="Data table pagination breaks when setting defaultPageIndex"
+                hash={3856}
+                totalComments={5}
+                totalPullRequests={1}
+                labels={[
+                    { name: 'bug' },
+                    { name: 'component: DataTable' },
+                    { name: 'good first issue' },
+                    { name: 'react' },
+                    { name: 'size: sand' },
+                ]}
+                avatars={[
+                    {
+                        alt: 'GitHub logo',
+                        src: 'https://avatars.githubusercontent.com/u/431533?s=64&u=9e71f9aaba1bd41361ee043ef018165b723ffc3e&v=4',
+                    },
+                ]}
+            />
+            <Row
+                title="Release Tracking"
+                hash={3856}
+                totalComments={5}
+                totalPullRequests={1}
+                labels={[
+                    { name: 'bug' },
+                    { name: 'release' },
+                    { name: 'update' },
+                    { name: 'react' },
+                    { name: 'size: boulder' },
+                    { name: 'warning' },
+                    { name: 'deprecation' },
+                ]}
+                avatars={[
+                    {
+                        alt: 'GitHub logo',
+                        src: 'https://avatars.githubusercontent.com/u/1863771?s=64&u=36c77cc9be0a64a196b503ce5e7fc335912d2bfa&v=4',
+                    },
+                    {
+                        alt: 'GitHub logo',
+                        src: 'https://avatars.githubusercontent.com/u/980622?s=64&u=1c3bf339aa927382a4f123b7b3669f847b1535e4&v=4',
+                    },
+                    {
+                        alt: 'GitHub logo',
+                        src: 'https://avatars.githubusercontent.com/u/40274682?s=64&u=e86e9e13f63066ced355e03ed21e05f91915567a&v=4',
+                    },
+                ]}
+            />
+        </Box>
     )
 }
 
@@ -145,6 +146,7 @@ function Row({
 }) {
     return (
         <Box
+            as="li"
             sx={{
                 display: 'contents',
 
