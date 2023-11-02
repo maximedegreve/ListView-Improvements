783f70697df037b3eb1c7fd885f04c06ddd10642
Maxime De Greve <maximedegreve@me.com>
2023-11-02 19:20:56 +0000

    Add more examples

--- src/Playground.js
+++ src/Playground.js
@@ -2,6 +2,8 @@ import {
     Box,
 } from '@primer/react'
 import TableV0 from './TableV0'
+import TableV0a from './TableV0a'
+import TableV0b from './TableV0b'
 import TableV1 from './TableV1'
 import TableV2 from './TableV2'
 import TableV3 from './TableV3'
@@ -24,6 +26,10 @@ function Playground() {
         >
             <Box sx={{maxWidth: 1400, textAlign: 'left', pb: 3,  color: 'fg.muted', fontSize: 1, width: '100%'}}>Example 0: No gaps 😍 (requires 0 values) (subgrid css)</Box>
             <TableV0/>
+            <Box sx={{maxWidth: 1400, textAlign: 'left', pb: 3, pt: 8, color: 'fg.muted', fontSize: 1, width: '100%'}}>Example 0a: No gaps 😍 (label below title) (subgrid css)</Box>
+            <TableV0a/>
+            <Box sx={{maxWidth: 1400, textAlign: 'left', pb: 3, pt: 8, color: 'fg.muted', fontSize: 1, width: '100%'}}>Example 0b: Gaps ☹️ (existing behaviour) (label below title) (subgrid css)</Box>
+            <TableV0b/>
             <Box sx={{maxWidth: 1400, textAlign: 'left', pb: 3, pt: 8,  color: 'fg.muted', fontSize: 1, width: '100%'}}>Example 1: No gaps 😍 (requires 0 values) (display:contents)</Box>
             <TableV1/>
             <Box sx={{maxWidth: 1400, textAlign: 'left', pb: 3, pt: 8, color: 'fg.muted', fontSize: 1, width: '100%'}}>Example 2: Gaps ☹️ (hidden 0 values) (subgrid css)</Box>
