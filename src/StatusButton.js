b514ed64a097fad92023018bac9d99cde2533612
Maxime De Greve <maximedegreve@me.com>
2023-11-03 18:27:16 +0000

    Add focus

--- src/StatusButton.js
+++ src/StatusButton.js
@@ -1,36 +1,42 @@
-import {
-    Box,
-    StyledOcticon,
-} from '@primer/react'
+import { Box, StyledOcticon } from '@primer/react'
 
-export default function StatusButton({count, icon}){
+export default function StatusButton({ count, icon }) {
     return (
         <Box
-        as="button"
-        disabled={count === 0}
-        sx={{
-            fontSize: 0,
-            color: count === 0  ? 'primer.fg.disabled' : 'fg.muted',
-            display: 'flex',
-            alignItems: 'center',
-            fontWeight: 'semibold',
-            bg: 'transparent',
-            border: 0,
-            ":hover:enabled": {
-                bg: 'actionListItem.default.hoverBg',
-                cursor: 'pointer',
-            },
-            px: 2,
-            py: 1,
-            borderRadius: 2,
-        }}
-    >
-        <StyledOcticon
-            icon={icon}
-            size={16}
-            sx={{ mr: 2, color: count === 0  ? 'primer.fg.disabled' : 'fg.muted' }}
-        />
-        {count}
-    </Box>
+            as="button"
+            disabled={count === 0}
+            sx={{
+                fontSize: 0,
+                color: count === 0 ? 'primer.fg.disabled' : 'fg.muted',
+                display: 'flex',
+                alignItems: 'center',
+                fontWeight: 'semibold',
+                bg: 'transparent',
+                border: 0,
+                transition: 'background .12s ease-out',
+
+                '&:active:not([disabled])': {
+                    bg: 'actionListItem.default.active',
+                    borderColor: 'btn.activeBorder',
+                },
+                ':hover:enabled': {
+                    bg: 'actionListItem.default.hoverBg',
+                    cursor: 'pointer',
+                },
+                px: 2,
+                py: 1,
+                borderRadius: 2,
+            }}
+        >
+            <StyledOcticon
+                icon={icon}
+                size={16}
+                sx={{
+                    mr: 2,
+                    color: count === 0 ? 'primer.fg.disabled' : 'fg.muted',
+                }}
+            />
+            {count}
+        </Box>
     )
 }
