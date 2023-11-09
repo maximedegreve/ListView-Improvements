fab015ace5c6dcfcdb283b048a1f815cda5519c4
Maxime De Greve <maximedegreve@me.com>
2023-11-09 19:10:08 +0000

    Fix

---
+++ src/Final/Header.js
@@ -0,0 +1,27 @@
+import { Box, Checkbox } from '@primer/react'
+
+export default function Header({ children, totalItems }) {
+    return (
+        <Box
+            sx={{
+                bg: 'canvas.subtle',
+                borderColor: 'border.default',
+                borderStyle: 'solid',
+                borderTopWidth: 1,
+                borderTopLeftRadius: 2,
+                borderTopRightRadius: 2,
+                display: 'flex',
+                borderLeftWidth: 1,
+                borderRightWidth: 1,
+                pl: 3,
+                pr: 2,
+                py: 2,
+                borderBottomWidth: 0,
+            }}
+        >
+            <Box sx={{display: 'flex', alignItems: 'center', pr: 3}}><Checkbox /></Box>
+            <Box sx={{flex: 1, display: 'flex', fontSize: 1, fontWeight: 'bold', alignItems: 'center'}}>{totalItems} items</Box>
+            <Box>{children}</Box>
+        </Box>
+    )
+}
--- src/Final/index.js
+++ src/Final/index.js
@@ -6,32 +6,41 @@ import Compact from './Compact'
 import CompactV2 from './CompactV2'
 import Default from './Default'
 import DefaultV2 from './DefaultV2'
+import Header from './Header'
 
 function Final() {
     const [selectedMode, setSelectedMode] = useState(0)
     const [fetchedData, setFetchedData] = useState(null)
 
-    const { show_repo, compact_item, default_item, repo_name, branch_name, show_branch, show_labels, notifications } =
-        useControls({
-            show_repo: false,
-            repo_name: 'primer/react',
-            branch_name: 'feature/update-login',
-            show_branch: false,
-            show_labels: true,
-            notifications: true,
-            compact_item: {
-                options: {
-                    'Version 2': 2,
-                    'Version 1': 1,
-                },
+    const {
+        show_repo,
+        compact_item,
+        default_item,
+        repo_name,
+        branch_name,
+        show_branch,
+        show_labels,
+        notifications,
+    } = useControls({
+        show_repo: false,
+        repo_name: 'primer/react',
+        branch_name: 'feature/update-login',
+        show_branch: false,
+        show_labels: true,
+        notifications: true,
+        compact_item: {
+            options: {
+                'Version 2': 2,
+                'Version 1': 1,
             },
-            default_item: {
-                options: {
-                    'Version 1': 1,
-                    'Version 2': 2,
-                },
+        },
+        default_item: {
+            options: {
+                'Version 1': 1,
+                'Version 2': 2,
             },
-        })
+        },
+    })
 
     useEffect(() => {
         fetch('https://api.github.com/repos/primer/react/issues', {
@@ -48,26 +57,24 @@ function Final() {
 
     return (
         <Box sx={{ width: '100%', maxWidth: 1400 }}>
-            <Box sx={{ pb: 4, display: 'flex' }}>
-                <Box sx={{ flex: 1 }}>
-                    <SegmentedControl
-                        aria-label="Mode"
-                        onChange={(index) => setSelectedMode(index)}
-                    >
-                        <SegmentedControl.Button selected={selectedMode === 0}>
-                            Default
-                        </SegmentedControl.Button>
-                        <SegmentedControl.Button selected={selectedMode === 1}>
-                            Compact
-                        </SegmentedControl.Button>
-                    </SegmentedControl>
-                </Box>
-            </Box>
+            <Header notifications={notifications} checkbox={true} totalItems={fetchedData?.length || 0 }>
+                <SegmentedControl
+                    aria-label="Mode"
+                    onChange={(index) => setSelectedMode(index)}
+                >
+                    <SegmentedControl.Button selected={selectedMode === 0}>
+                        Default
+                    </SegmentedControl.Button>
+                    <SegmentedControl.Button selected={selectedMode === 1}>
+                        Compact
+                    </SegmentedControl.Button>
+                </SegmentedControl>
+            </Header>
             <Box
                 as="ul"
                 sx={{
                     display: 'grid',
-                    gridTemplateColumns:[
+                    gridTemplateColumns: [
                         'auto 1fr auto',
                         'auto 1fr auto',
                         '1fr auto auto auto',
@@ -75,18 +82,15 @@ function Final() {
                     ],
                     mx: 'auto',
                     borderColor: 'border.default',
-                    borderRadius: 2,
+                    borderBottomLeftRadius: 2,
+                    borderBottomRightRadius: 2,
                     borderWidth: 1,
                     borderStyle: 'solid',
                     boxShadow: 'shadow.small',
-                    "li:first-child": {
-                        borderTopLeftRadius: 2,
-                        borderTopRightRadius: 2,
-                    },
-                    "li:last-child": {
+                    'li:last-child': {
                         borderBottomLeftRadius: 2,
                         borderBottomRightRadius: 2,
-                    }
+                    },
                 }}
             >
                 {fetchedData?.map((item, index) => {
